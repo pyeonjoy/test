3f4d02b1ececc22394d8694d6f8488645a04f524
joy <ppee1220@gmail.com>
2024-02-06 17:37:29 +0900

    수정

---
+++ src/com/ict/day01/Ex01.java
@@ -0,0 +1,15 @@
+package com.ict.day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello git");
+		System.out.println("Hello git");
+		System.out.println("Hello git");
+		System.out.println("Hello git");
+		
+	}
+}
---
+++ src/com/ict/day01/Ex03.java
@@ -0,0 +1,9 @@
+package com.ict.day01;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println("세번째");
+		System.out.println("세번째");
+		System.out.println("세번째");
+	}
+}
